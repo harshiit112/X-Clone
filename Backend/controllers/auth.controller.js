7b37a825205a72edb2f3292ad772a92c515438d1
harshitit112 <hv702383@gmail.com>
2025-05-04 23:48:41 +0530

    feat: initialize frontend with Vite and React setup
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Developed App.jsx as the main component with state management.
    - Added React logo SVG in assets directory.
    - Established global styles in index.css.
    - Set up main entry point in main.jsx for rendering the application.
    - Configured Vite with vite.config.js for React support.

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,71 +1,103 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
 import User from "../models/user.model.js";
 
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-    try {
-        const {fullName, username, email, password} = req.body;
+  try {
+    const { fullName, username, email, password } = req.body;
 
-        const emailRegex = /^[^s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if(!emailRegex.text(email)) {
-            return res.status(400).json({ error: "Invalid email format" });
-        }
-
-        const existingUser = await User.findOne({ username });
-        if(existingUser) {
-            return res.status(400).json({ error: "Username is already taken" })
-        }
-
-        const existingEmail = await User.findOne({ email });
-        if(existingEmail) {
-            return res.status(400).json({ error: "Email is already taken" })
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        const newUser = new User({
-            fullName,
-            username,
-            email,
-            password:hashedPassword
-        })
-
-        if(newUser) {
-            generateTokenAndSetCookie(newUser._id,res)
-            await newUser.save();
-
-            res.status(201).json({
-                _id: newUser._id,
-                fullName: newUser.fullName,
-                username: newUser.username,
-                email: newUser.email,
-                followers: newUser.followers,
-                following: newUser.following,
-                profileImg: newUser.profileImg,
-                coverImg: newUser.coverImg,
-            })
-        } else {
-            res.status(400).json({ error: "Invalid user data"})
-        }
-
-    } catch (error) {
-        console.log("Error in signup controller", error.message);
-        
-
-        res.status(500).json({ error: "Internal Server Error "})
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
     }
-}
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password has atleast 6 character" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error " });
+  }
+};
 
 export const login = async (req, res) => {
-    res.json({
-        data: "You hit the login endpoint",
+  try {
+    const {username,password} = req.body;
+    const user = await user.findOne({username});
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+    
+
+    if(!user || !isPasswordCorrect) {
+        return res.status(400).json({ error: "Invalid username or password"})
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        email: user.email,
+        following: user.following,
+        followers: user.followers,
+        profileImg: user.profileImg,
+        coverImg: user.coverImg,
     });
-}
+
+
+
+
+
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error " });
+  }
+};
 
 export const logout = async (req, res) => {
-    res.json({
-        data: "You hit the logout endpoint",
-    });
-}
+  res.json({
+    data: "You hit the logout endpoint",
+  });
+};
