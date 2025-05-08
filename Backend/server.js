05ac749ebaa9bb1ef78e2e58cf1d50f5af270d71
harshitit112 <hv702383@gmail.com>
2025-05-08 17:45:08 +0530

    feat: implement notification retrieval and deletion functionality

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notificaton.route.js"
 import connectMongoDB from "./db/connect.MongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
