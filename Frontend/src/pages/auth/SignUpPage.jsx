a888ccae60313c4274639e37a438ff25ce59ddf7
harshitit112 <hv702383@gmail.com>
2025-05-10 22:46:55 +0530

    feat: add QueryClientProvider to main component and update import for XSvg in SignUpPage

--- Frontend/src/pages/auth/SignUpPage.jsx
+++ Frontend/src/pages/auth/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-import Xsvg from "../../component/svgs/X";
+import XSvg from "../../component/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
