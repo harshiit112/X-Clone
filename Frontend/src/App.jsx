3938825a15f7c6079f242b3d4636864e4e6caeff
harshitit112 <hv702383@gmail.com>
2025-05-13 18:42:08 +0530

    feat: add Sidebar, RightPanel, and Posts components; update HomePage layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,15 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/auth/SignUpPage";
+import Sidebar from "./component/common/Sidebar";
+import RightPanel from "./component/common/RightPanel";
 
 function App() {
   return (
-    <div data-theme="x" className="min-h-screen bg-base-100 text-white">
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar /> 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
+      <RightPanel /> 
     </div>
   );
 }
