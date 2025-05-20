e3531327e604cd12c893936960545e0c57a2a372
harshitit112 <hv702383@gmail.com>
2025-05-20 15:43:12 +0530

    feat: extend month names in formatMemberSinceDate utility for improved date formatting

--- Frontend/src/utils/date/index.js
+++ Frontend/src/utils/date/index.js
@@ -25,6 +25,16 @@ export const formatMemberSinceDate = (createdAt) => {
 	const months = [
 		"January",
 		"February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
 	];
 	const month = months[date.getMonth()];
 	const year = date.getFullYear();
